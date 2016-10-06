6a211170724867c6d6baaf7acd392627cbaeaa53
Kathy Higgins <khiggins@ehsms.com>
2016-10-06 11:27:11 -0600

    completed setting up firebase

---
+++ Pods/Target Support Files/Pods-unused-social/Pods-unused-social-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_unused_socialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_unused_socialVersionString[];
+
---
+++ unused-social/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  unused-social
+//
+//  Created by Kathy Higgins on 10/6/16.
+//  Copyright © 2016 ColoApps. All rights reserved.
+//
+
+import UIKit
+
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
