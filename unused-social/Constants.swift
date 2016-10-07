fac9f28fdb7f83e88ac2f0930c1b2bcc338dc602
Kathy Higgins <khiggins@ehsms.com>
2016-10-07 15:46:18 -0600

    Sign in ui custom classes complete

---
+++ unused-social/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  unused-social
+//
+//  Created by Kathy Higgins on 10/7/16.
+//  Copyright © 2016 ColoApps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255
